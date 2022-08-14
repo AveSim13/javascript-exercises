6ad373bc186b1ceec85facc35bea10fa23b1cef2
Avery Simmons <avesim13@gmail.com>
2022-08-14 13:26:55 -0600

    Worked through exercises one through four and got all associated tests to pass.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    var heyString = '';
+    while (num > 0) {
+        heyString += str;
+        num--;
+    }
+    if (num < 0) {
+        heyString = 'ERROR';
+    }
+    return heyString;
 };
 
 // Do not edit below this line
